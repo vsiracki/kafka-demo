4694d9c72d2288f7ee9e7aba6b7635ed9f872a23
delinquents <v.velja@yahoo.com>
2023-10-18 11:11:58 +0200

    Added missing pom.xml

---
+++ kafka-to-elastic-service/src/main/java/com/htec/microservices/to/elastic/service/transformer/AvroToElasticModelTransformer.java
@@ -0,0 +1,28 @@
+package com.htec.microservices.to.elastic.service.transformer;
+
+import com.htec.microservices.elastic.model.index.impl.TwitterIndexModel;
+import com.htec.microservices.kafka.avro.model.TwitterAvroModel;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AvroToElasticModelTransformer {
+
+    public List<TwitterIndexModel> getElasticModels(List<TwitterAvroModel> avroModels) {
+        return avroModels.stream()
+                .map(avroModel -> TwitterIndexModel
+                        .builder()
+                        .userId(avroModel.getUserId())
+                        .id(String.valueOf(avroModel.getId()))
+                        .text(avroModel.getText())
+                        .createdAt(ZonedDateTime.ofInstant(Instant.ofEpochMilli(avroModel.getCreatedAt()),
+                                ZoneId.systemDefault()))
+                        .build()
+                ).collect(Collectors.toList());
+    }
+}
